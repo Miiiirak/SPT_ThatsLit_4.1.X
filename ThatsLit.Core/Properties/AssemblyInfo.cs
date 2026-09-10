ca8f6a38c6c2eff22c7deaf280ba4c2b67487bff
Hunter Baker <hunterbaker046@gmail.com>
2026-09-09 23:15:08 -0500

    patch: sync AssemblyInfo VersionChecker attribute to EFT 40743

--- ThatsLit.Core/Properties/AssemblyInfo.cs
+++ ThatsLit.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using DrakiaXYZ.VersionChecker;
+using DrakiaXYZ.VersionChecker;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using static ThatsLit.AssemblyInfo;
@@ -16,4 +16,4 @@ using ThatsLit;
 [assembly: Guid("d08f8f91-95cf-4aa5-b7d8-f5d58f2feabb")]
 [assembly: AssemblyVersion(ModVersion)]
 [assembly: AssemblyFileVersion(ModVersion)]
-[assembly: VersionChecker(AssemblyInfo.TarkovVersion)]
+[assembly: VersionChecker(40743)]
